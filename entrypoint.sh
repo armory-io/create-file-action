6ef183822dc4df7166f345e710c2172d8baefd6d
Jason McIntosh <jason.mcintosh@armory.io>
2020-01-07 14:22:57 -0600

    Using input_ for variables...

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [[ ! -z "${FILE_NAME}" ]]; then
-  DIR=`dirname $FILE_NAME`
+if [[ ! -z "${INPUT_FILE_NAME}" ]]; then
+  DIR=`dirname $INPUT_FILE_NAME`
   mkdir -p $DIR
 else
   echo "No file parameter set... exiting..."
   exit -1
 fi
 
-if [[ -z "${FILE_BASE64}" ]]; then
-  echo "Using FILE_DATA to write to $FILE_NAME"
-  echo $FILE_DATA > "$FILE_NAME"
+if [[ -z "${INPUT_FILE_BASE64}" ]]; then
+  echo "Using INPUT_FILE_DATA to write to $INPUT_FILE_NAME"
+  echo $INPUT_FILE_DATA > "$INPUT_FILE_NAME"
 else
-  echo "Using FILE_BASE64 to write to $FILE_NAME"
-  echo $FILE_BASE64 | base64 --decode > $FILE_NAME
+  echo "Using INPUT_FILE_BASE64 to write to $INPUT_FILE_NAME"
+  echo $INPUT_FILE_BASE64 | base64 --decode > $INPUT_FILE_NAME
 fi
