c3a641b51f29b00d1cecee0818176aa2fa418e6a
Jason McIntosh <jason.mcintosh@armory.io>
2020-01-07 14:06:43 -0600

    Added directory support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ -z "${FILE_NAME}" ]]; then
+  DIR=$(dirname $FILE_NAME)
+  mkdir -p $DIR
+else
+  echo "No file parameter set... exiting..."
+  exit -1
+fi
+
 if [[ -z "${FILE_BASE64}" ]]; then
   echo "Using FILE_DATA to write to $FILE_NAME"
   echo $FILE_DATA > "$FILE_NAME"
